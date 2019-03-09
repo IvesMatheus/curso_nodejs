07bcf427a9f66dc155ed542a9a3af9782c0e8709
Ives Matheus <ives@meliuz.com.br>
2019-03-09 15:40:07 -0400

    Salva noticia no BD e redireciona pra página de noticias

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,12 +1,23 @@
-module.exports = function(app){
-	app.get('/formulario_inclusao_noticia', function(req, res){
+module.exports = function(application){
+	application.get('/formulario_inclusao_noticia', function(req, res){
 		res.render("admin/form_add_noticia");
 	});
 
-	app.post('/noticias/salvar', function(req, res){
+	application.post('/noticias/salvar', function(req, res){
 		// recupera informações enviadas via POST
-		const noticias = req.body;
-		res.send(noticias);
+		const noticia = req.body;
+
+		// recupera conexão como banco de dados
+		var connection = application.config.dbConnection();
+		//recupera model que possui métodos de acesso a BD
+		var noticiasModel = application.app.models.noticiasModel;
+
+		noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+			// método redireciona pra outra página do projeto.
+			// Assim evita de ao usuário clicar em F5, reenvie o formulário,
+			// fazendo uma nova inserção no BD
+			res.redirect('/noticias');
+		});
 	});
 }
 
